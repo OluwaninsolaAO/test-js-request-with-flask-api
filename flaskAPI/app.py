a7c6c3f7016a54c6cef537d800ad81c862d6abec
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-27 11:31:31 +0100

    Update FlaskAPI to use flask's own jsonify in place of json module

--- flaskAPI/app.py
+++ flaskAPI/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """simple flask API endpoint"""
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from user import User
 
 
@@ -11,15 +11,19 @@ app.url_map.strict_slashes = False
 users = ['Abraham', 'Isaac', 'Jacob', 'Joseph']
 all_users_dict = [User(user).__dict__ for user in users]
 
+@app.route('/')
+def home():
+    """index page / root url"""
+    return "Hello World!"
+
 @app.route('/users', methods=['GET', 'PUT'])
 def users():
     """Returns a simple json representation of User"""
-    import json
 
     if request.method == 'GET':
-        return json.dumps(all_users_dict)
+        return jsonify(all_users_dict)
     elif request.method == 'PUT':
         return "Recieved"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
