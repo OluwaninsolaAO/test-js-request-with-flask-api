c875ae97bfaa5071ec80fc362c6f9c1637ba00cd
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-27 11:13:33 +0100

    Make 00-index prettier

--- 00-index.js
+++ 00-index.js
@@ -7,6 +7,7 @@ const request = require('request');
 
 request('http://localhost:5000/users', function (error, response, body) {
 	console.log('error:', error);
-	console.log('body:', body);
-	console.log('statusCode:', response && response.statusCode);
+	console.log('\nbody:', body);
+	console.log('\nbody as json:', JSON.parse(body));
+	console.log('\nstatusCode: ', response && response.statusCode);
 });
